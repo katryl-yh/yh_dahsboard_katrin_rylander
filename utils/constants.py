b6354623d15eb9a4ca481c420c069d6c9d4f3f0f
Katrin katryl-yh <katrin@rylander.eu>
2025-08-16 21:26:50 +0200

    updated constants.py to match new version of main

--- utils/constants.py
+++ utils/constants.py
@@ -1,8 +1,20 @@
 from pathlib import Path
 
-DATA_DIRECTORY = Path(__file__).parents[1] / "data" / "resultat_kurser"
+# Project & data paths
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+DATA_DIRECTORY = PROJECT_ROOT / "data" / "resultat_kurser"
 
-if __name__ == "__main__":
-    print("\n" *2)
-    print(DATA_DIRECTORY)
-    print("\n" *2)
+# Filenames & sheets
+EXCEL_RESULTS_FILE = "resultat-2025-for-kurser-inom-yh.xlsx"
+EXCEL_RESULTS_SHEET = "Lista ansökningar"
+
+EXCEL_APPS_FILE = "inkomna-ansokningar-2025-for-kurser.xlsx"
+EXCEL_APPS_SHEET = "Lista ansökningar"
+
+# Columns & prefixes
+REQUIRED_COLUMNS = {"Län", "Beslut", "Utbildningsområde"}
+KEY_COL = "Diarienummer"
+SOKT_PREFIX = "Sökt antal platser"
+
+COL_TOTAL_SOKTA = "Totalt antal sökta platser"
+COL_TOTAL_BEVILJADE_PLATSER = "Totalt antal beviljade platser"
