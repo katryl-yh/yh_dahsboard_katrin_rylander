d3ce92747ff5f38a5f8bc8256710aa4a9f223dd2
Katrin katryl-yh <katrin@rylander.eu>
2025-08-22 00:27:56 +0200

    finalized storytelling page

--- frontend/pages/storytelling.py
+++ frontend/pages/storytelling.py
@@ -1,18 +1,48 @@
 import taipy.gui.builder as tgb
+from utils.constants import PROJECT_ROOT
 
-image_education_area = "../assets/storytelling/insights_education_area.png"
-image_admitted_students = "../assets/storytelling/insights_admitted_students.png"
+image_education_area = f"{PROJECT_ROOT}/assets/storytelling/insights_education_area.png"
+image_admitted_students = f"{PROJECT_ROOT}/assets/storytelling/insights_admitted_students.png"
 
 
 with tgb.Page() as storytelling:
     with tgb.part(class_name="page-container"):
-        tgb.navbar()
-        with tgb.part(class_name="title-card"):
-            tgb.text("# Datainsikter", mode="md")
-    
-        with tgb.part(style="margin-bottom: 40px;"):
-                tgb.image("{image_education_area}", width="80%")
-        with tgb.part(style="margin-bottom: 40px;"):
-                tgb.image("{image_admitted_students}", width="80%")
+        with tgb.part(class_name="dashboard-content card stack-large"):
+            tgb.navbar()
             
- 
+            with tgb.part(class_name="card"):
+                tgb.text("## Datainsikter", mode="md")
+                tgb.text("Visualiseringar av data för att hitta strategiska möjligheter.", mode="md")
+                
+                # First insight
+                with tgb.part(class_name="insight-section"):
+                    tgb.text("### Strategiska nischområden", mode="md")
+                    tgb.image("{image_education_area}", width="80%")
+                    
+                    # Comment section for the first insight
+                    with tgb.part(class_name="insight-comment"):
+                        tgb.text("#### Insikter:  \n"
+                        "- Att komplettera kursutbudet med utbildningar inom mer nischade områden kan vara en framgångsfaktor.  \n "
+                        "- Diagrammet, baserat på MYH:s statistik, visar tydligt att nischade utbildningsområden ofta har högre beviljandegrad.  \n",
+                        mode="md")
+                
+                # Second insight
+                with tgb.part(class_name="insight-section"):
+                    tgb.text("### Könsfördelning inom utbildningsområden", mode="md")
+                    tgb.image("{image_admitted_students}", width="80%")
+
+                    # Comment section for the second insight
+                    with tgb.part(class_name="insight-comment"):
+                        tgb.text("#### Insikter:  \n"
+                        "- I yrkeshögskolan finns en tydlig ambition att minska könssegregeringen i utbildningar.  \n"
+                        "- MYH arbetar aktivt för breddad rekrytering genom att uppmuntra utbildningsanordnare att locka fler från det underrepresenterade könet.  \n"
+                        "- Diagrammet visar områden där aktiva insatser skulle kunna förbättra könsbalansen.  \n",
+                        mode="md")
+
+                    with tgb.part(class_name="insight-resources"):
+                        tgb.text("#### MYH erbjuder stöd:  \n"
+                        "- Kommunikationshandledning med tips om inkluderande marknadsföring  \n"
+                        "- Stödmaterial för jämställd utbildning och rekrytering  \n"
+                        "- Möjlighet till rådgivning och anordnarstöd  \n"
+                        "- Tillgång till profilerings- och informationsmaterial för både kurser och program  \n",
+                        mode="md")
